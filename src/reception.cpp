8b48f59d80390c3560a95b61e2ec8a0f2b2fa4de
Thibault FOUCOU <thibault.foucou@epitech.eu>
2019-04-28 02:27:55 +0200

    check argument

--- src/reception.cpp
+++ src/reception.cpp
@@ -17,8 +17,21 @@ Recept::~Recept()
 {
 }
 
+int check_quantity(std::vector<std::string> tab)
+{
+    return -1;
+}
+
 int check_size(std::vector<std::string> tab)
 {
+    if (tab[1].compare("S") == 0 || tab[1].compare("M") == 0)
+        check_quantity(tab);
+    else if (tab[1].compare("L") == 0 || tab[1].compare("XL") == 0)
+        check_quantity(tab);
+    else if (tab[1].compare("XXL") == 0)
+        check_quantity(tab);
+    else
+        return -1;
     return -1;
 }
 
@@ -27,7 +40,7 @@ int check_command(std::string str)
     std::vector<std::string> argument = split_arg(str);
 
     if (argument[0].compare("status") == 0)
-        std::cout << "yes" << std::endl;
+        std::cout << "yes martin travaille connard" << std::endl;
     else if (argument[0].compare("Margaritana") == 0 || argument[0].compare("Regina") == 0)
         check_size(argument);
     else if (argument[0].compare("Americana") == 0 || argument[0].compare("Fantasia") == 0)
